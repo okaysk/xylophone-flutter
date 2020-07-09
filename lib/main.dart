b7c0bbe8b4777c28290434567ab320a1be922080
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-09 12:33:36 +0900

    audio_cache & using method

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
